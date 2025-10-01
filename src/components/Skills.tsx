af53681005a5d758714e0b3537f7cc631f02321e
MA-Silent <38739@ma-web.nl>
2025-10-01 19:33:36 +0200

    changed transition speed skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -24,7 +24,7 @@ export default function Skills({ array, id }: { array: Array<skill>, id: string 
     <section ref={ref} className="h-screen snap-center grid gap-5 items-center place-items-center overflow-auto group" id={id} style={{ gridTemplateColumns: `repeat(${(window.innerWidth > 768) ? Math.min(6, array.length) : Math.min(2, array.length)}, minmax(0, 1fr))` }} >
       {array.map((skill, idx) => {
         return (
-          <div className="p-4 bg-gray-600 text-center flex flex-col items-center justify-center rounded-2xl gap-2 mt-12 group-[.visible]:translate-x-0 -translate-x-[100vw] transition-all duration-400 group-[.not-visible]:transition-[none] ease-out" style={{transitionDelay: `${idx*100}ms`}} key={idx}>
+          <div className="p-4 bg-gray-600 text-center flex flex-col items-center justify-center rounded-2xl gap-2 mt-12 group-[.visible]:translate-x-0 -translate-x-[100vw] transition-all duration-300 group-[.not-visible]:transition-[none] ease-out" style={{transitionDelay: `${idx*100}ms`}} key={idx}>
             {skill.imageSrc ? <img src={skill.imageSrc} alt="skillImage" className="size-32" /> : null}
             {skill.title}
           </div>
