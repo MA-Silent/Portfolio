6ecb1758349184e2bdd38182dc61f6dbe63f40d6
MA-Silent <38739@ma-web.nl>
2025-10-07 09:56:33 +0200

    Revamped Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useEffect, useRef, useState } from "react";
 import { type project } from "../util/constants"
-import { X, User, Users, GithubIcon } from "lucide-react";
+import { X, User, Users } from "lucide-react";
 
 const observer = new IntersectionObserver((entries) => {
     for (const entry of entries) {
@@ -23,33 +23,35 @@ export default function Projects({ projectArray, id }: { projectArray: Array<pro
     }
 
     return (
-        <section className="h-screen snap-center grid gap-12 items-center place-items-center overflow-auto max-sm:grid-cols-2 group" id={id} style={(window.innerWidth >= 768) ? { gridTemplateColumns: `repeat(${Math.min((projectArray.length == 4) ? 2 : 3, projectArray.length)}, minmax(0, 1fr))` } : {}} ref={reference}>
-            {projectArray.map((project, idx) => {
-                const icon = (project.group) ? <div className="text-amber-400"><Users /></div> : <div className="text-sky-400"><User /></div>
-                return (
-                    <Fragment key={idx}>
-                        <button className="cursor-pointer md:opacity-80 md:hover:opacity-100 duration-200 ease-in-out" onClick={() => handleClick(idx)}>
-                            <div className="bg-gray-600 p-2 scale-0 transition-all duration-600 group-[.visible]:scale-100">
-                                <div className="md:absolute bg-neutral-600/60 w-full py-1 -inset-x-0 justify-between flex flex-row px-3">
-                                    <div className="">{project.title}</div>
-                                    {icon}
+        <section className="w-full flex justify-center items-center snap-center h-screen">
+            <section className="flex flex-wrap max-w-8/12 gap-8 justify-center items-center place-items-center group max-h-screen py-16 overflow-auto" id={id} style={(window.innerWidth >= 768) ? { gridTemplateColumns: `repeat(${Math.min((projectArray.length == 4) ? 2 : 3, projectArray.length)}, minmax(0, 1fr))` } : {}} ref={reference}>
+                {projectArray.map((project, idx) => {
+                    const icon = (project.group) ? <div className="text-amber-400"><Users /></div> : <div className="text-sky-400"><User /></div>
+                    return (
+                        <Fragment key={idx}>
+                            <button className="cursor-pointer md:opacity-80 md:hover:opacity-100 duration-200 ease-in-out" onClick={() => handleClick(idx)}>
+                                <div className="bg-gray-600 p-2 scale-0 transition-all duration-600 group-[.visible]:scale-100">
+                                    <div className="md:absolute bg-neutral-600/60 w-full py-1 -inset-x-0 justify-between flex flex-row px-3">
+                                        <div className="">{project.title}</div>
+                                        {icon}
+                                    </div>
+                                    <img src={project.image} alt="Project Image" className="md:w-2xs md: md:pb-2" />
                                 </div>
-                                <img src={project.image} alt="Project Image" className="md:w-2xs md: md:pb-2" />
-                            </div>
-                        </button>
+                            </button>
 
-                        <section className={` transition-all duration-200 fixed left-1/2 top-1/2 -translate-1/2 w-1/2 ${openPopup == idx ? "visible opacity-100" : "invisible opacity-0 -translate-y-full"} flex flex-col gap-2 justify-between bg-gray-600/80 rounded p-2 z-30`}>
-                            <div className="flex justify-between w-full">
-                                <h2 className="flex flex-row gap-2">{project.title} {icon}  {<a href={project.link}><GithubIcon /></a>}</h2>
-                                <button onClick={() => setOpenPopup(undefined)} className="cursor-pointer"><X className="size-8 md:stroke-zinc-500 md:hover:stroke-white transition-colors ease-in-out" /></button>
-                            </div>
-                            <div>
-                                {project.description ? project.description : "No Description available for this project at this moment"}
-                            </div>
-                        </section>
-                    </Fragment>
-                )
-            })}
+                            <section className={` transition-all duration-200 fixed left-1/2 top-1/2 -translate-1/2 w-1/2 ${openPopup == idx ? "visible opacity-100" : "invisible opacity-0 -translate-y-full"} flex flex-col gap-2 justify-between bg-gray-600/80 rounded p-2 z-30`}>
+                                <div className="flex justify-between w-full">
+                                    <h2 className="flex flex-row gap-2 text-xl">{project.title} {icon}  {<a href={project.link}>GIT</a>}</h2>
+                                    <button onClick={() => setOpenPopup(undefined)} className="cursor-pointer"><X className="size-8 md:stroke-zinc-500 md:hover:stroke-white transition-colors ease-in-out" /></button>
+                                </div>
+                                <div className="font-light">
+                                    {project.description ? project.description : "No Description available for this project at this moment"}
+                                </div>
+                            </section>
+                        </Fragment>
+                    )
+                })}
+            </section>
         </section>
     )
 }
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -12,29 +12,29 @@ export type skill = {
 }
 
 export const projects: Array<project> = [
-    {image: "projects/1.png", title: "React + Express + mySQL", link: "https://github.com/MA-Silent/FlexIntro/", description: "A simple website made wtih React for the frontend and ExpressJS + mySQL for the backend, that allows the user to drag the images on the page to another snap point"},
-    {image: "projects/2.png", title: "Portfolio", link: "https://github.com/MA-Silent/Portfolio", description: "This website made for me to get a job"},
-    {image: "projects/4.png", title: "ExpressJs API + mySQL", link: "https://github.com/MA-Silent/FLEX-expressjs-Api/", description: "An express API that connects to a mySQL database"},
-    {image: "projects/3.png", title: "React Basics", link: "https://github.com/MA-Silent/M8-React/", description:"Hello World"},
-    {image: "logos/DiscordJS.png", title: "Discord.js Bot", link:"https://github.com/Ben200002/Discord.js", description: "A Discord bot made with the Discord.js library that i made because i wanted to challenge my javascript skill"}
+  { image: "projects/1.png", title: "React + Express + mySQL", link: "https://github.com/MA-Silent/FlexIntro/", description: "A simple website made wtih React for the frontend and ExpressJS + mySQL for the backend, that allows the user to drag the images on the page to another snap point" },
+  { image: "projects/2.png", title: "Portfolio", link: "https://github.com/MA-Silent/Portfolio", description: "This website made for me to get a job" },
+  { image: "projects/4.png", title: "ExpressJs API + mySQL", link: "https://github.com/MA-Silent/FLEX-expressjs-Api/", description: "An express API that connects to a mySQL database" },
+  { image: "projects/3.png", title: "React Basics", link: "https://github.com/MA-Silent/M8-React/", description: "Hello World" },
+  { image: "logos/DiscordJS.png", title: "Discord.js Bot", link: "https://github.com/Ben200002/Discord.js", description: "A Discord bot made with the Discord.js library that i made because i wanted to challenge my javascript skill" },
 ]
 
 export const skills: Array<skill> = [
-    {title: "Linux", imageSrc: "logos/linuxlogo.svg"},
-    {title: "NodeJS", imageSrc: "logos/nodelogo.svg"},
-    {title: "React", imageSrc: "logos/reactlogo.png"},
-    {title: "mySQL", imageSrc: "logos/mySQLlogo.svg"},
-    {title: "C#", imageSrc: "logos/cslogo.svg"},
-    {title: "Typescript", imageSrc: "logos/tslogo.svg"},
-    {title: "Javascript", imageSrc: "logos/jslogo.svg"},
-    {title: "Vite", imageSrc: "logos/vitelogo.svg"},
-    {title: "Python", imageSrc: "logos/pythonlogo.svg"},
-    {title: "Git", imageSrc: "logos/gitlogo.svg"},
-    {title: "ExpressJS", imageSrc: "logos/expresslogo.svg"},
-    {title: "Tailwind CSS", imageSrc: "logos/tailwindlogo.svg"},
-    {title: "Docker CLI", imageSrc: "logos/dockerlogo.svg"},
-    {title: "HTML", imageSrc: "logos/htmllogo.svg"},
-    {title: "CSS", imageSrc: "logos/csslogo.svg"},
+  { title: "Linux", imageSrc: "logos/linuxlogo.svg" },
+  { title: "NodeJS", imageSrc: "logos/nodelogo.svg" },
+  { title: "React", imageSrc: "logos/reactlogo.png" },
+  { title: "mySQL", imageSrc: "logos/mySQLlogo.svg" },
+  { title: "C#", imageSrc: "logos/cslogo.svg" },
+  { title: "Typescript", imageSrc: "logos/tslogo.svg" },
+  { title: "Javascript", imageSrc: "logos/jslogo.svg" },
+  { title: "Vite", imageSrc: "logos/vitelogo.svg" },
+  { title: "Python", imageSrc: "logos/pythonlogo.svg" },
+  { title: "Git", imageSrc: "logos/gitlogo.svg" },
+  { title: "ExpressJS", imageSrc: "logos/expresslogo.svg" },
+  { title: "Tailwind CSS", imageSrc: "logos/tailwindlogo.svg" },
+  { title: "Docker CLI", imageSrc: "logos/dockerlogo.svg" },
+  { title: "HTML", imageSrc: "logos/htmllogo.svg" },
+  { title: "CSS", imageSrc: "logos/csslogo.svg" },
 ];
 
 export const AboutText: string = `My name is Ben Ruitenbeek, and I’m a 17-year-old Dutch software developer. I’m currently in my third year of MBO at Mediacollege Amsterdam, studying Software Development. I discovered programming when I was 13, and since then I’ve been focused on improving my skills and learning as much as I can. Skipping a grade allowed me to move through my studies faster and dedicate more time to developing practical and efficient software solutions. \n\n Outside of school, I like to relax while keeping an eye on the projects I’m currently working on. Whenever I spot a problem, I make sure to fix it as quickly as possible. This approach has helped me stay engaged with my work and continually improve my skills. In the future, I would like to continue my studies in Cyber Security, where I hope to apply my technical skills to build secure and reliable systems.`;
