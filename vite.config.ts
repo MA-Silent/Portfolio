dbe5f5963c2488c99f0b7cfdef331170dee07991
MA-Silent <38739@ma-web.nl>
2025-11-03 12:06:54 +0100

    changed optimization

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,21 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    ViteImageOptimizer(),
+    ViteImageOptimizer({
+         // Enable WebP generation for all images
+         webp: {
+           quality: 80, // Adjust as needed (0–100)
+         },
+         // Optionally convert other formats too
+         jpg: {
+           quality: 80,
+         },
+         jpeg: {
+           quality: 80,
+         },
+         png: {
+           quality: 80,
+         },
+       }),
   ],
 })
